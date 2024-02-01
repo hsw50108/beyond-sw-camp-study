2ee0ddd78e652171a6e31c13f76df0b605e1e6b4
dongwook <hsw50108@gmail.com>
2024-02-01 15:07:14 +0900

    add : workshop 숫자 맞추기

---
+++ src/workshop/workshop1/guessnumber/GuessGame.java
@@ -0,0 +1,17 @@
+package workshop.workshop1.guessnumber;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame {
+
+    public static int getUserInput() {
+        System.out.println("***********************************");
+        System.out.println("10번의 기회가 있습니다.");
+        System.out.println("1에서 100사이의 숫자를 맞춰보세요.");
+        System.out.println("***********************************");
+
+        Random rand = new Random();
+        return rand.nextInt(100) + 1;
+    }
+}
