78fe01aef5e0474e5a838c0bb705b297614d24b5
dongwook <hsw50108@gmail.com>
2024-02-19 17:24:40 +0900

    add : db 설정

--- src/MvcMain.java
+++ src/MvcMain.java
@@ -14,14 +14,12 @@ public class MvcMain {
 		// 사용자의 입력화면을 통해서 게시글 작성을 요청하는 영역
 		InsertController insert = new InsertController() ;
 
-		// 사용자가 입력한 값을 dto에 담아서 ctroller 전달
-		/*
+		// 사용자가 입력한 값을 dto에 담아서 controller 전달
 		PostRequestDto params = new PostRequestDto();
 		params.setTitle("불금");
 		params.setContent("간호해야함");
-		params.setWriter("섭섭해"); 
-		int flag = insert.execute(params); 
-		*/
+		params.setWriter("섭섭해");
+		int flag = insert.execute(params);
 		/*
 		Map<String, String> map = new HashMap<String, String>() ;  
 		map.put("title", "불필요한 금요일") ;
