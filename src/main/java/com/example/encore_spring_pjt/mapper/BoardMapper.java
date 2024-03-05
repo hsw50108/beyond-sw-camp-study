3ad3037176e6b77ef99153a8418e6e9d5fe9d009
dongwook <hsw50108@gmail.com>
2024-03-05 16:42:30 +0900

    add : soft-delete, findAll, count

--- src/main/java/com/example/encore_spring_pjt/mapper/BoardMapper.java
+++ src/main/java/com/example/encore_spring_pjt/mapper/BoardMapper.java
@@ -4,6 +4,8 @@ import com.example.encore_spring_pjt.domain.BoardRequest;
 import com.example.encore_spring_pjt.domain.BoardResponse;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /*
 * encore_board_tbl 과 CRUD 작업을 위한 추상메서드 선언
 * mapper는 기존의 dao와 동일한 작업을 진행하는 것
@@ -12,5 +14,13 @@ import org.apache.ibatis.annotations.Mapper;
 public interface BoardMapper {
     void save(BoardRequest params);
     BoardResponse findByIdx(BoardRequest params);
+    void updateByIdx(BoardRequest params);
 
+    // 레코드 건수 확인
+    int count();
+    // soft delete
+    void deleteByIdx(BoardRequest params);
+
+    // select (multi finder)
+    List<BoardResponse> findAll();
 }
--- src/test/java/com/example/encore_spring_pjt/mapper/BoardMapperTest.java
+++ src/test/java/com/example/encore_spring_pjt/mapper/BoardMapperTest.java
@@ -2,11 +2,13 @@ package com.example.encore_spring_pjt.mapper;
 
 import com.example.encore_spring_pjt.domain.BoardRequest;
 import com.example.encore_spring_pjt.domain.BoardResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 class BoardMapperTest {
@@ -16,13 +18,13 @@ class BoardMapperTest {
 
     @Test
     public void save() {
-        System.out.println("BoardMapperTest.save" + boardMapper);
+//        System.out.println("BoardMapperTest.save" + boardMapper);
 
         BoardRequest boardRequest = new BoardRequest();
 
-        boardRequest.setTitle("orm class");
-        boardRequest.setContent("mybatis mapping");
-        boardRequest.setWriter("encore");
+        boardRequest.setTitle("orm class3");
+        boardRequest.setContent("mybatis mapping3");
+        boardRequest.setWriter("encore3");
         boardRequest.setNoticeYn(true);
         boardRequest.setSecretYn(false);
 
@@ -30,13 +32,61 @@ class BoardMapperTest {
     }
 
     @Test
-    void findByIdx() {
+    void findByIdx() throws Exception {
         System.out.println("BoardMapperTest.findById");
         BoardRequest boardRequest = new BoardRequest();
         boardRequest.setIdx(1);
         BoardResponse response = boardMapper.findByIdx(boardRequest);
+
         System.out.println("BoardMapperTest.find result : ");
         System.out.println(response);
+        System.out.println("BoardMapperTest.find json result : ");
+        String boardJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(response);
+        System.out.println(boardJson);
+    }
+
+    @Test
+    void updateByIdx() throws Exception {
+        System.out.println("BoardMapperTest.findById");
+
+        BoardRequest boardRequest = new BoardRequest();
+        boardRequest.setIdx(1);
+        BoardResponse response = boardMapper.findByIdx(boardRequest);
+
+        if (response != null) {
+            boardRequest.setTitle("update3");
+            boardRequest.setContent("update content");
+            boardRequest.setWriter("update by dongwook");
+            boardMapper.updateByIdx(boardRequest);
+        } else {
+            throw new IllegalArgumentException("회원을 찾을 수 없습니다.");
+        }
+    }
+
+    @Test
+    void count() {
+        System.out.println("BoardMapperTest.count");
+        int count = boardMapper.count();
+        System.out.println("count : " + count);
+    }
+
+    @Test
+    void deleteByIdx() {
+        System.out.println("BoardMapperTest.deleteByIdx");
+        BoardRequest boardRequest = new BoardRequest();
+        boardRequest.setIdx(2);
+        boardMapper.deleteByIdx(boardRequest);
+    }
+
+    @Test
+    void findAll() {
+        System.out.println("BoardMapperTest.findAll");
+
+        List<BoardResponse> list = boardMapper.findAll();
+
+        for (BoardResponse response : list) {
+            System.out.println(response);
+        }
     }
 
 }
