cc2f6832d6a2fb103eecfe75e4abaf2a10dd2d2f
dongwook <hsw50108@gmail.com>
2024-04-29 14:13:37 +0900

    add: test controller commit

--- leafy-backend/src/main/java/com/devwiki/leafy/controller/plant/PlantController.java
+++ leafy-backend/src/main/java/com/devwiki/leafy/controller/plant/PlantController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+// controller commit
 @RestController
 @RequestMapping("/api/v1/plants")
 @RequiredArgsConstructor
